18fc507828378948c770c34527048879be2d7230
Danylo Pohrebniak <ppddee777@gmail.com>
2022-12-14 14:37:58 +0200

    hot fix

--- offlineWatch/apps.py
+++ offlineWatch/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class OfflinewatchConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'offlineWatch'
+
+    try:
+        import offlineWatch.signals
+    except ImportError:
+        pass
